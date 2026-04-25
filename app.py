56cc31d869bdef384ed04043ea09f8a8bfc14066
Neeraj Panwar <neerajpanwar844@gmail.com>
2026-04-25 12:30:49 +0530

    Fixed Display

--- app.py
+++ app.py
@@ -6,45 +6,78 @@ from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(page_title="DigitDetective",page_icon='🔍',layout='wide')
 st.title("DigitDetective")
+st.badge("numpy only - no frameworks")
 st.space('medium')
+@st.cache_data
+def load_model():
+    n_net = Neural()
+    n_net.load_weights()
+    return n_net
+
+model = load_model()
 
 def process_img(img):
     img = cv2.cvtColor(img.astype('uint8'),cv2.COLOR_RGBA2GRAY) 
     ret,thresh_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
     counters,hierarchy = cv2.findContours(thresh_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnt = max(counters)
+    cnt = max(counters,key=cv2.contourArea)
     x,y,w,h = cv2.boundingRect(cnt)
     crop = thresh_img[y:y+h,x:x+w]
     pad_img = cv2.copyMakeBorder(crop,int(0.2*h),int(0.2*h),int(0.2*w),int(0.2*w),cv2.BORDER_CONSTANT,value=[0,0,0])
     img_array = cv2.resize(pad_img,(28,28),interpolation=cv2.INTER_AREA)
     _, thresh_img2 = cv2.threshold(img_array,127,255,cv2.THRESH_BINARY)
-    fin_img = cv2.imwrite(f'output.jpg',thresh_img2)
     norm_array = thresh_img2/255
     return norm_array.flatten()
 
-def guess(canvas_result):
-    if canvas_result.json_data is not None:
-        objects = canvas_result.json_data.get("objects")
-        if len(objects) > 0:
-            st.success("Drawing detected!")
-            img = canvas_result.image_data
-            array = process_img(img)
-            n_net = Neural()
-            n_net.load_weights()
-            result = n_net.forward(array)
-            result = (result.round()).tolist()[0]
-            st.write(f"Is your number {result.index(max(result))} ?")
-            print(result)
-            
-        else:
-            st.info("Canvas is currently empty.")
-            
-center = st.container(border=True,width='stretch',height='content')
-with center:
-    """## Draw a number"""
+def predict(model,array):
+    result = model.forward(array)
+    predicted_digit = np.argmax(result)
+    confidence = result
+    return predicted_digit,confidence
 
-    canvas_result = st_canvas(fill_color="#f22e1f",stroke_width=20,stroke_color="white",background_color="#000000",width=400,height=400)
-    guess(canvas_result)
+            
+left_col, right_col = st.columns(2)
+left_col = left_col.container(border=True, height="stretch", vertical_alignment="top")
+with left_col:
+    """## CANVAS"""
+
+    canvas_result = st_canvas(stroke_width=20,stroke_color="white",background_color="#000000",width=400,height=400,display_toolbar=False)
+    button_cols = st.columns(2)
+    with button_cols[1]:
+        predict_btn = st.button("Predict", key="draw_predict")
+    with button_cols[0]:
+        clear_btn = st.button("Clear", key="draw_clear")    
+
+    if predict_btn and len(canvas_result.json_data.get("objects")) > 0:
+        array = process_img(canvas_result.image_data)
+        digit, confidence = predict(model,array)
+        st.session_state['prediction'] = [digit,confidence]
+    
+    elif clear_btn and len(canvas_result.json_data.get("objects")) > 0:
+        pass
+
+right_col = right_col.container(border=True, height="stretch", vertical_alignment="top")
+with right_col:
+    st.header("PREDICTION")
+    if 'prediction' in st.session_state :
+        result = st.session_state['prediction']
+        st.markdown(f'**<p style="font-size: 70px;">{result[0]}</p>**', unsafe_allow_html=True,text_alignment='center')
+        data = result[1].tolist()
+        data = data[0]
+        for i,val in enumerate(data):
+            col1,col2,col3 = st.columns(3)
+            with col1:
+                st.write(f"**{i}**")
+            with col2:
+                st.progress(value = val)
+            with col3:
+                st.write(f'{val:.2e}%')
+        with col1:
+            st.metric('Confidence',f'{(max(data)*100):.2f}')
+        with col2:
+            st.metric('Model','EMNIST')
+        with col3:
+            st.metric('Accuracy','97.8%')
 
 
     
